d83bf8f093ab2d8c3e39c753342c914fa8cbd572
Антон Фролов <F-Miralius@yandex.ru>
2024-03-26 22:36:31 +0400

    Greatest common divisor task's solution

---
+++ GreatestCommonDivisor/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A{};
+    int B{};
+
+    cin >> A >> B;
+
+    while (A > 0 && B > 0)
+    {
+        if (A > B)
+        {
+            A %= B;
+        }
+        else
+        {
+            B %= A;
+        }
+    }
+
+    cout << A + B;
+
+    return 0;
+}
